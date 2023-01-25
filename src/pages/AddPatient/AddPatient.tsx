2478ee2914143efb2b91353248c52c3bb34d624d
metalheadpnz <metalheadpnz@gmail.com>
2023-01-25 18:27:38 +0300

    add Formik

--- src/pages/AddPatient/AddPatient.tsx
+++ src/pages/AddPatient/AddPatient.tsx
@@ -4,6 +4,16 @@ import s from './AddPatient.module.scss'
 import {Input2 as Input} from "../../components/Input2/Input2";
 import {Select} from "../../components/Select/Select";
 import {ReactComponent as Arrow} from "../../../src/assets/images/arrow.svg";
+import {Formik} from "formik";
+import {Button} from "../../components/Button/Button";
+import * as Yup from 'yup';
+
+const SignupSchema = Yup.object().shape(
+    {
+        firstName: Yup.string().min(2, 'Too Short').max(10, 'too long').required('Required')
+    }
+)
+
 
 export const AddPatient = () => {
     const navigation = useNavigate()
@@ -15,7 +25,7 @@ export const AddPatient = () => {
         <div className={s.wrap}>
             <div className={s.breadCrumbs}>
                 <span onClick={changeLocationHandler}>Patients</span>
-                    <Arrow style={{rotate: "90deg", position: 'relative', bottom: "2px"}}/>
+                <Arrow style={{rotate: "90deg", position: 'relative', bottom: "2px"}}/>
                 <span>Add patient</span>
             </div>
 
@@ -24,26 +34,79 @@ export const AddPatient = () => {
                 <div className={s.fromLabel}>Patient Information</div>
                 <div className={s.patientInformation}>
 
-                    <Input label={'First Name'} require/>
-                    <Input label={'Date of Birth *'}/>
-                    <Input label={'Secondary Phone'}/>
-                    <Input label={'Address/Street 2'}/>
-                    <Input label={'Zip Code'}/>
-                    <Input label={'Email'}/>
-                    <Input label={'Race'} isError={true} errorMessage={'sdf'}/>
-                    <Input label={'Last Name *'}/>
+
+                    <Formik
+                        initialValues={{firstName: "", Email: "", dateOfBirth: ''}}
+                        validationSchema={SignupSchema}
+                        onSubmit={(values) => {
+                            console.log(values)
+                        }}>
+                        {
+                            (formik) => {
+
+                                const {
+                                    values,
+                                    errors,
+                                    touched,
+                                    handleChange,
+                                    handleBlur,
+                                    handleSubmit,
+                                    isSubmitting
+                                } = formik
+
+                                console.log(formik)
+                                return (
+                                    <form onSubmit={handleSubmit}>
+                                        <Input
+                                            name={'firstName'}
+                                            label={'First Name'}
+                                            require
+                                            value={values.firstName}
+                                            onChange={handleChange}
+                                        />
+                                        <Input
+                                            name={'Email'}
+                                            label={'Email'}
+                                            require
+                                            value={values.Email}
+                                            onChange={handleChange}
+                                        />
+                                        <Input
+                                            name={'dateOfBirth'}
+                                            onChange={handleChange}
+                                            value={values.dateOfBirth}
+                                            type={'date'}
+                                            label={'Date of Birth'}
+                                        />
+                                        <button type='submit'>btn</button>
+                                    </form>
+                                )
+                            }
+                        }
+                    </Formik>
+
+
+                    {/*<Input label={'First Name'} require/>*/}
+                    {/*<Input label={'Date of Birth *'}/>*/}
+                    {/*<Input label={'Secondary Phone'}/>*/}
+                    {/*<Input label={'Address/Street 2'}/>*/}
+                    {/*<Input label={'Zip Code'}/>*/}
+                    {/*<Input label={'Email'}/>*/}
+                    {/*<Input label={'Race'} isError={true} errorMessage={'sdf'}/>*/}
+                    {/*<Input label={'Last Name *'}/>*/}
                     {/*<Select title={'Sex *'} options={[{name: 'Male'},{name: 'Female'},]}/>*/}
                     {/*<Select title={'Country'} options={[{name: 'Rus'},{name: 'USA'},]}/>*/}
                     {/*<Select title={'City'} options={[{name: 'Pnz'},{name: 'Msk'},]}/>*/}
-                    <Input label={'SSN, last 4 digits'}/>
-                    <Input label={'Ethnicity'}/>
-                    <Input label={'Middle Name'}/>
-                    <Input label={'Cell Phone *'}/>
-                    <Input label={'Address/Street 1'}/>
+                    {/*<Input label={'SSN, last 4 digits'}/>*/}
+                    {/*<Input label={'Ethnicity'}/>*/}
+                    {/*<Input label={'Middle Name'}/>*/}
+                    {/*<Input label={'Cell Phone *'}/>*/}
+                    {/*<Input label={'Address/Street 1'}/>*/}
                     {/*<Select title={'State'} options={[{name: 'TX'}, {name: 'CA'}, {name: 'FL'}]}/>*/}
-                    <Input label={'Race'} isError={true} errorMessage={'sdf'}/>
-                    <Select title={'State'} options={[{name: 'TX'}, {name: 'CA'}, {name: 'FL'}]}/>
-                    <Select title={'State'} options={[{name: 'TX'}, {name: 'CA'}, {name: 'FL'}]}/>
+                    {/*<Input label={'Race'} isError={true} errorMessage={'sdf'}/>*/}
+                    {/*<Select title={'State'} options={[{name: 'TX'}, {name: 'CA'}, {name: 'FL'}]}/>*/}
+                    {/*<Select title={'State'} options={[{name: 'TX'}, {name: 'CA'}, {name: 'FL'}]}/>*/}
+
                 </div>
 
             </div>
